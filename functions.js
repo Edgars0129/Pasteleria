137aa82c2fff59555497ad33847c8186f08f871e
Edgars0129 <adgarh4@outlook.com>
2024-08-18 16:04:38 -0600

    Correciones menus

--- functions.js
+++ functions.js
@@ -6,16 +6,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const productItems = document.querySelectorAll('.product-item');
 
     // Menú móvil
-    menuToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', function(event) {
+        event.stopPropagation();
         mobileMenu.classList.toggle('active');
     });
 
-    // Cerrar el menú al hacer clic fuera de él
     document.addEventListener('click', function(event) {
         if (!mobileMenu.contains(event.target) && !menuToggle.contains(event.target)) {
             mobileMenu.classList.remove('active');
         }
-});
+    });
+
+    // Cerrar menú al hacer clic en los enlaces
+    const mobileMenuLinks = document.querySelectorAll('.mobile-menu a');
+    mobileMenuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            mobileMenu.classList.remove('active');
+        });
+    });
 
     // Funcionalidad del modal de producto
     productItems.forEach(item => {
@@ -46,12 +54,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const products = {
             '1': {
                 name: 'Producto 1',
-                image: './img/producto1.jpg',
+                image: './img/skincare1.jpg',
                 description: 'Descripción detallada del Producto 1. Aquí puedes incluir información sobre sus características, beneficios, etc.'
             },
             '2': {
                 name: 'Producto 2',
-                image: './img/producto2.jpg',
+                image: './img/skincare2.jpg',
                 description: 'Descripción detallada del Producto 2. Esta es una descripción de ejemplo que puedes personalizar para cada producto.'
             },
             // Agrega más productos según sea necesario
